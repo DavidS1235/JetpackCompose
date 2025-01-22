4cc4b449541ed2a6cf2f5b3d85505fd21163a2c0
David Silva <davidsilvacordova@gmail.com>
2025-01-21 20:23:50 -0500

    feat: DS-006 playing with spacers

--- MyFirstComposeApps/app/src/main/java/com/ds/myfirstcomposeapps/components/layout/MyComplexLayout.kt
+++ MyFirstComposeApps/app/src/main/java/com/ds/myfirstcomposeapps/components/layout/MyComplexLayout.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MyComplexLayout(modifier: Modifier) {
@@ -23,6 +27,7 @@ fun MyComplexLayout(modifier: Modifier) {
         ) {
             Text("Ejemplo 1")
         }
+        Spacer(Modifier.height(20.dp))
         Row(
             modifier = Modifier
                 .weight(1f)
@@ -36,6 +41,7 @@ fun MyComplexLayout(modifier: Modifier) {
             ) {
                 Text("Ejemplo 2")
             }
+            Spacer(Modifier.width(20.dp))
             Box(
                 modifier = Modifier
                     .weight(1f)
