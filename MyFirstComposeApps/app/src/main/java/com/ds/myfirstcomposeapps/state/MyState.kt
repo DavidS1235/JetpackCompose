b7c0f31f9100bf828e854278b8fe691271942836
David Silva <davidsilvacordova@gmail.com>
2025-01-24 22:35:13 -0500

    feat: DS-011 playing with advanced states

--- MyFirstComposeApps/app/src/main/java/com/ds/myfirstcomposeapps/state/MyState.kt
+++ MyFirstComposeApps/app/src/main/java/com/ds/myfirstcomposeapps/state/MyState.kt
@@ -4,16 +4,19 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
+//by State property works as Lombok get/set, so no need to explicit .getValue
 fun MyState(modifier: Modifier) {
-    var number = remember { mutableStateOf(0) }
+    var number by rememberSaveable() { mutableIntStateOf(0) }
     Column {
-        Text("Pulsame: ${number.value}", Modifier.clickable { number.value += 1 })
-        Text("Pulsame: ${number.value}", Modifier.clickable { number.value += 1 })
+        Text("Pulsame: ${number}", Modifier.clickable { number += 1 })
+        Text("Pulsame: ${number}", Modifier.clickable { number += 1 })
     }
 
 }
